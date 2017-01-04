6c0cd80c33fadf7adf773f223bb58ca1d7734e68
zheverson <zheverson@qq.com>
2017-01-04 17:25:20 +0800

    sasa

--- src/client/app/Content/docComponent.jsx
+++ src/client/app/Content/docComponent.jsx
@@ -2,32 +2,73 @@ import React from 'react';
 
 const boxStyle = {
     width: 400,
-    margin: '40px auto 0'
+    margin: '40px auto 0',
+    borderCollapse: 'collapse',
+    borderStyle: 'hidden'
 }
 
 const formStyle = {
     margin: 10
-}
+};
+
+
+const tdStyle = {
+    textAlign: 'center',
+    padding: 5
+};
+
+const thStyle = {
+    textAlign: 'center',
+    padding: 5,
+};
+
 
 class DocComponent extends React.Component {
     render() {
         return (
-            <div style={boxStyle}>
-                <SingleDocComponent name="客服工作流程" url="http://54.223.65.44:8100/static/file/ibd/xianhuo.csv"/>
-                <SingleDocComponent name="打单流程" url="http://54.223.65.44:8100/static/file/ibd/xianhuo.csv"/>
-                <SingleDocComponent name="上现货" url="http://54.223.65.44:8100/static/file/ibd/xianhuo.csv"/>
-            </div>
+            <table style={boxStyle}>
+                <tr>
+                    <th style={thStyle}>
+                        文档名称
+                    </th>
+                    <th style={thStyle}>
+                        上次修改时间
+                    </th>
+                    <th style={thStyle}>
+                        下载
+                    </th>
+                </tr>
+                <tbody>
+                    <SingleDocComponent name="客服工作流程" date="1月4日 16点" url="http://54.223.65.44:8100/static/file/ibd/xianhuo.csv"/>
+                    <SingleDocComponent name="打单流程" date=" " url="http://54.223.65.44:8100/static/file/ibd/xianhuo.csv"/>
+                    <SingleDocComponent name="上现货" date=" " url="http://54.223.65.44:8100/static/file/ibd/xianhuo.csv"/>
+                </tbody>
+
+            </table>
         );
     }
 }
 
+
 class SingleDocComponent extends React.Component {
     render() {
         return (
-            <form method="get" action={this.props.url} style={formStyle}>
-                {this.props.name}
-                <button type="submit" style={{marginLeft: 8}}>下载</button>
-            </form>
+            <tr>
+                <td style={tdStyle}>
+                    {this.props.name}
+                </td>
+                <td style={tdStyle}>
+                    {this.props.date}
+                </td>
+                <td style={tdStyle}>
+                    <form method="get" action={this.props.url}>
+                        <button type="submit">
+                            下载
+                        </button>
+                    </form>
+                </td>
+            </tr>
+
         );
     }
 }
